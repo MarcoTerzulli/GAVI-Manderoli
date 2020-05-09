8bf63ecf1316a10da0d779149cca70e20c25fa13
Riccardo Mescoli <244993@studenti.unimore.it>
2020-05-09 03:20:35 +0200

    Rifacimento file Indexing.py e Parsing.py (ex xml_parsing.py) + commenti

--- Indexing.py
+++ Indexing.py
@@ -12,13 +12,18 @@ index_dir = "indexdir"
 
 def indexWriter_creator():
 
+    # Creazione dello schema dei documenti da indicizzare
     schema = Schema(title=TEXT(stored=True), identifier=ID(stored=True), content=TEXT(stored=True))
 
+    # Verifica dell'esistenza della cartella dell'indice
     if not os.path.exists(index_dir):
+        # In caso la cartella non esista viene creata
         os.mkdir(index_dir)
 
+    # Creazione dell'indice all'interno della cartella designata
     index = create_in(index_dir,schema,)
 
+    # La funzione restituisce un oggetto in grado di inserire (scrivere) documenti all'interno dell'indice
     return index.writer()
 
 
