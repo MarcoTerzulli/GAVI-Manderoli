bfe29fa49b331a0958dde7b9cf7fb18d85b006bd
Riccardo Mescoli <244993@studenti.unimore.it>
2020-07-02 20:40:44 +0200

    Aggiunto metodo per generare snippet

--- searching.py
+++ searching.py
@@ -7,6 +7,7 @@
 from whoosh.index import EmptyIndexError, open_dir
 from whoosh.qparser import OrGroup, QueryParser
 from whoosh.searching import Results, Hit
+import re
 
 from configuration import index_dir
 
@@ -36,19 +37,38 @@ class WikiSearcherModule:
         results: Results = self.__searcher.search(query, limit=n_results)
 
         # Restituisco la lista dei risultati
-        return results
+        return self.__results_setup(results)
 
-    @staticmethod
-    def get_similar_articles(base_result):
+    def get_similar_articles(self, base_result):
 
         try:
             assert isinstance(base_result, Hit)
         except AssertionError:
             raise TypeError
 
-        return base_result.more_like_this('content')
+        return self.__results_setup(base_result.more_like_this('content'))
+
+    def get_result_highlights(self, result):
+        cleaned_content = result['content']
+        highlights = self.__cleanhtml(result.highlights('content', text=cleaned_content, top=5))
+        highlights = highlights
+        print(highlights)
+        return highlights
 
     @staticmethod
     def get_article_url(title):
         relative_url = "".join([c if c != " " else "_" for c in title])
         return "https://en.wikipedia.org/wiki/" + relative_url
+
+    @staticmethod
+    def __cleanhtml(raw_html):
+        cleanr = re.compile(r'<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});|\[\[|\]\]|\{\{|\}\}')
+        cleantext = re.sub(cleanr, '', raw_html)
+        return cleantext
+
+    @staticmethod
+    def __results_setup(results):
+        results.fragmenter.charlimit = 500
+        results.fragmenter.surround = 50
+
+        return results
